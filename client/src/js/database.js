6fc31f5b301dff02c1c056fd5d95971210be7065
Alfred Garraffa <agarraffa@gmail.com>
2022-05-10 21:51:42 -0600

    updated logic. page loads but the actual editor and install aren't working

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,48 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+
+  console.log('Running putDb')
+
+  // establishes contact with the db
+  const jateDb = await openDB('jate', 1);
+
+  //creates a transaction
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  // opens the object store
+  const store = tx.objectStore('jate');
+
+  // updates the data
+  const request = store.put({ text: content});
+
+  // confirm request
+  const result = await request;
+  
+  console.log('Data updated', result);
+  
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
 
+  console.error('Running getDb');
+
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+
+  console.log('result.value', result);
+  return result;
+
+};
+
+// starts the db
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
@@ -18,11 +20,53 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new MiniCssExtractPlugin(),
+      new WorkboxPlugin.GenerateSW(),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Al's Text Editor",
+        short_name: 'TextEd',
+        description: "Al's relatively ok text editor",
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons')
+          },
+        ]
+      })
     ],
 
     module: {
       rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
         
       ],
     },
